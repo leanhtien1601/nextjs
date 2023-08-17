245cbc33dec1034be78ff0495927e30c931717cc
leanhtiendzt <tienla.dzt@gmail.com>
2023-08-18 00:00:19 +0700

    init

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+export const metadata = {
+  title: "Theads",
+  description: "A Next.js 13 Meta",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
